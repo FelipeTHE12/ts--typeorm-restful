772924d61108594ff1c21543fc3ac05fffc865b4
Felipe <felipeoliveirasilva12@outlook.com>
2022-03-23 11:48:14 -0300

    feat: temporary error middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,29 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes/mainRouter';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Server error',
+    });
+  },
+);
 
 app.listen(3000, () => {
   console.log('Ola');
