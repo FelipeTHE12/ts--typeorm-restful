bfe17a7045e93c2bc6d17efb26ff1689c6d625cf
Felipe <felipeoliveirasilva12@outlook.com>
2022-05-11 01:31:22 -0300

    feat(PRODUCTS): crud services

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class CreateProductService {
+  protected async execute({
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is a product with the same name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+export default class DeleteProductService {
+  protected async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product doenst exist');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
